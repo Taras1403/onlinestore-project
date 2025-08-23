e4bcddeba56e9bc34e00d15f0cef0e3c1a140a8d
Mac <mac@MacBook-Air.local>
2025-08-23 17:03:35 +0300

    wrapping CartProvider in AuthProvider

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CartProvider } from './context/CartContext';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <CartProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </CartProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
