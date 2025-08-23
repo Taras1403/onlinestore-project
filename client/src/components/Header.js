8978c5de417c88fc38b7f0af7ff4c02a50e49fd3
Mac <mac@MacBook-Air.local>
2025-08-23 16:55:25 +0300

    Add Exit btn

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 import './Header.css';
 
 const Header = () => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+
   return (
     <header className="header">
       <h1><Link to="/">Online store</Link></h1>
       <nav>
         <Link to="/cart">Shop cart</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {isLoggedIn ? (
+          <button onClick={handleLogout}>Exit</button>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
