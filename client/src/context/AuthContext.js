d40faa2bb7db2ad572b5a9f13554e3caeb325b1f
Mac <mac@MacBook-Air.local>
2025-08-28 19:03:49 +0300

    Add fakeUser

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -5,23 +5,33 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+  const [user, setUser] = useState(null); 
 
   useEffect(() => {
     setIsLoggedIn(!!token);
+    if (token) {
+      const fakeUser = {
+        name: 'User',
+        image: './images/user.svg'
+      };
+      setUser(fakeUser);
+    }
   }, [token]);
 
-  const login = (newToken) => {
+  const login = (newToken, userData) => {
     localStorage.setItem('token', newToken);
     setToken(newToken);
+    setUser(userData);
   };
 
   const logout = () => {
     localStorage.removeItem('token');
     setToken(null);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ token, isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ token, isLoggedIn, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
