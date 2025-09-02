b01b1513127989fc081a905d49727383a6f3cb44
Mac <mac@MacBook-Air.local>
2025-09-02 22:21:49 +0300

    Adding navigation-links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect, useRef } from 'react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import './Header.css';
@@ -9,6 +10,7 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);  // Pop-up menu
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const dropdownRef = useRef(null);
+  const [openMenu, setOpenMenu] = useState(null);
 
   const handleLogout = () => {
     logout();
@@ -40,12 +42,41 @@ const Header = () => {
         setIsSearchOpen(!isSearchOpen);
   };
 
+  const handleMenuToggle = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <header className="header">
-        <div className="logo-container">
-          <Link to="/"><img src="./images/logo.svg" alt="Logo" className="logo-icon" /></Link>
-          <h1><Link to="/">Online store</Link></h1>
-        </div>
+
+        <nav className="header-nav">
+          <div className="logo-container">
+            <Link to="/"><img src="./images/logo.svg" alt="Logo" className="logo-icon" /></Link>
+            <h1><Link to="/">Online store</Link></h1>
+          </div>
+          <div className="navigation-links">
+            
+            {/* Пункт з випадаючим меню */}
+            <div className="dropdown-container">
+              <button className="navig-link-button" onClick={handleMenuToggle}>
+                <span>Brands</span>
+                {openMenu ? <FaChevronUp /> : <FaChevronDown />}
+              </button>
+              {openMenu && (
+                <div className="dropdown-menu">
+                  <Link to="/brands/apple">Apple</Link>
+                  <Link to="/brands/samsung">Samsung</Link>
+                  <Link to="/brands/google">Google</Link>
+                </div>
+              )}
+            </div>
+          </div>
+          <Link to="/catalog" className="navig-1">Catalog</Link>
+          <Link to="/special-offers" className="navig-2">Special Offers</Link>
+          <Link to="/social-networks" className="navig-3">Social Networks</Link>
+          <Link to="/about-us" className="navig-4">About us</Link>
+        </nav>
+
         <nav>
           {isSearchOpen ? (
                 <div className="search-container">
@@ -56,6 +87,7 @@ const Header = () => {
             )}
 
           <Link to="/"><img src="./images/question.svg" alt="Question" className="question-icon" /></Link>
+          <Link to="/"><img src="./images/follow.svg" alt="Follow Cart" className="follow-icon" /></Link>
           <Link to="/cart"><img src="./images/shop.svg" alt="Shop Cart" className="cart-icon" /></Link>
           
           {user ? (
@@ -71,7 +103,15 @@ const Header = () => {
           ) : (
             <>
               <Link to="/login">Log in</Link>
-              <Link to="/register">Sign up</Link>
+              <Link to="/register" style={{ 
+                backgroundColor: 'transparent', 
+                color: 'black', 
+                border: '2px solid black', 
+                borderRadius: '5px', 
+                padding: '7px 14px',
+                transition: 'all 0.3s ease' }}>
+                  Sign up
+              </Link>
             </>
           )}
         </nav>
